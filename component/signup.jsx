fd7bd03e5c7ee1e0eeac168d06b7582b4f498cac
Fatin-Shahriare-Sium <sium1206@gmail.com>
2021-10-11 00:08:53 +0600

    done,some issues ,alhamdulillah

---
+++ component/signup.jsx
@@ -0,0 +1,34 @@
+
+
+const Singup = () => {
+    return (
+        <div className='signup-wrapper'>
+            <p className='signup-wrapper__head'>Signup</p>
+            <div className="singup-wrapper__form">
+
+                <form>
+                    <div className="single-input__box">
+                        <p>Name</p>
+                        <input type="text" />
+                    </div>
+                    <div className="single-input__box">
+                        <p>Email</p>
+                        <input type="email" />
+                    </div>
+                    <div className="single-input__box">
+                        <p>Password</p>
+                        <input type="passowrd" />
+                    </div>
+                </form>
+                <div>
+                    <p>ujyh</p>
+                </div>
+            </div>
+            <div className="signup-wrapper__img">
+
+            </div>
+        </div>
+    )
+}
+
+export default Singup;
---
+++ pages/x/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Singup from '../../component/signup'
+
+const index = () => {
+    return (
+        <div>
+            <Singup />
+
+        </div>
+    )
+}
+
+export default index;
